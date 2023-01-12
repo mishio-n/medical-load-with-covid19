0116ab51b1fb242f4be14667d78bfde1e8478b87
mishio-n <doncool340@gmail.com>
2023-01-13 00:20:29 +0900

    指定エリアの円周描画用の座標を生成

--- sam-app-ca-16-medical-facility/api/get-rate-by-point/main.go
+++ sam-app-ca-16-medical-facility/api/get-rate-by-point/main.go
@@ -23,6 +23,32 @@ type FacilityWithRate struct {
 type Response struct {
 	AreaRate   float64            `json:"areaRate"`
 	Facilities []FacilityWithRate `json:"facilities"`
+	GeoJson    []Coordinates      `json:"geoJson"`
+}
+
+type Coordinates struct {
+	Longitude float64
+	Latitude  float64
+}
+
+type AreaGeoJson struct {
+	Type     string `json:"type"`
+	Features []struct {
+		Type       string `json:"type"`
+		Properties struct {
+			Type          string  `json:"type"`
+			StrokeColor   string  `json:"strokeColor"`
+			StrokeOpacity int     `json:"strokeOpacity"`
+			StrokeWeight  int     `json:"strokeWeight"`
+			FillColor     string  `json:"fillColor"`
+			FillOpacity   float64 `json:"fillOpacity"`
+			MarkerColor   string  `json:"marker-color"`
+		} `json:"properties,omitempty"`
+		Geometry struct {
+			Type        string      `json:"type"`
+			Coordinates [][]float64 `json:"coordinates"`
+		} `json:"geometry"`
+	} `json:"features"`
 }
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -57,9 +83,12 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		}, nil
 	}
 
+	circlePoints := generateCirclePoints(Coordinates{Longitude: longitude, Latitude: latitude}, distance)
+
 	response := new(Response)
 	response.Facilities = facilities
 	response.AreaRate = calcAreaRate(facilities)
+	response.GeoJson = circlePoints
 
 	body, _ := json.Marshal(response)
 
@@ -131,4 +160,46 @@ func calcAreaRate(facilities []FacilityWithRate) float64 {
 	}
 
 	return math.Floor(sum/float64(len(facilities))*100) / 100
+}
+
+// 指定エリアの円を作成するための座標群を作成する
+// @see https://www.nanchatte.com/map/circle.html
+func generateCirclePoints(center Coordinates, radius float64) []Coordinates {
+	// 赤道半径(m) (WGS-84)
+	const EQUATORIAL_RADIUS = 6378137
+
+	// 扁平率の逆数 : 1/f (WGS-84)
+	const F = 298.257223
+
+	// 離心率の２乗
+	E := ((2 * F) - 1) / math.Pow(F, 2)
+
+	// 赤道半径 × π
+	const PI_ER = math.Pi * EQUATORIAL_RADIUS
+
+	// 1 - e^2 sin^2 (θ)
+	TMP := 1 - E*math.Pow(math.Sin(center.Latitude*math.Pi/180), 2)
+
+	// 経度１度あたりの長さ(m)
+	arc_lat := (PI_ER * (1 - E)) / (180 * math.Pow(TMP, 3/2))
+
+	// 緯度１度あたりの長さ(m)
+	arc_lon := (PI_ER * math.Cos(center.Latitude*math.Pi/180)) / (180 * math.Pow(TMP, 1/float64(2)))
+
+	// 半径をｍ単位に
+	R := radius * 1000
+
+	var points []Coordinates
+	for i := 0; i <= 360; i++ {
+		rad := float64(i) / 180 * math.Pi
+		lat := (R/arc_lat)*math.Sin(rad) + center.Latitude
+		lon := (R/arc_lon)*math.Cos(rad) + center.Longitude
+		points = append(points, Coordinates{Latitude: lat, Longitude: lon})
+	}
+
+	return points
+}
+
+func formatGeoJson(circlePoints []Coordinates, facilityPoints []Coordinates) {
+
 }
